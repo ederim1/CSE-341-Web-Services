d147da7b60c84cb2f802439831d603a3a1fd5a4d
ederim1 <ederim@byui.edu>
2023-04-22 23:53:00 -0600

    Adding code

---
+++ server.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send('Eder Morales');
+});
+ 
+const port = 3000;
+
+app.listen(process.env.port || port);
+console.log('Web server is listening at port ' + (process.env.port || port));
